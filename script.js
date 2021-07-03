7970d2c4dcc0001bbfc93e22704f048edda15a79
ameer khan <ameerkhankhan33.ak@gmail.com>
2021-07-03 16:39:39 +0530

    new qoute feature added

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 let quotesArr = [];
 let onQuote = 0;
+const quoteContainer = document.querySelector('#quote-container');
+const getQuoteBtn = document.querySelector('#new-quote');
+const quoteTextEle = document.querySelector('#quote');
+const quoteAuthorEle = document.querySelector('#author');
+const getTwitterBtn = document.querySelector('#twitter');
+const loader = document.querySelector('#loader');
+
+
 // Get Quote from API
-async function getQuote() {
+async function getQuote() { 
+       loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
         const data = await response.json();
         console.log(data);
         quotesArr = [...quotesArr, ...data];
-        return quotesArr;
+        return quotesArr;   
     } catch (error) {
         // getQuote();   
         console.log('whoops, no quote', error);
     }
 }  
 
-getQuote();
+getQuote().then(() => {
+    setTimeout(complete, 2000)
+});
 
 // On Load
 // getQuote();
 
-const getQuoteBtn = document.querySelector('#new-quote');
-const quoteTextEle = document.querySelector('#quote');
-const quoteAuthorEle = document.querySelector('#author');
-const getTwitterBtn = document.querySelector('#twitter');
 console.log(getTwitterBtn);  
+
+//show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading 
+function complete() { 
+     console.log(!loader.hidden)
+    if(!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 const twitterUrl = 'https://twitter.com/intent/tweet';
 getTwitterBtn.href = `${twitterUrl}?text=${quoteTextEle.innerText}`
